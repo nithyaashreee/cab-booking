59e31408f8f64c0c95371230a0167e4aeed7f43b
Nithyashree D <nithyashreed777@gmail.com>
2026-02-13 04:59:30 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,13 @@
 window._config = {
+    cognito: {
+userPoolId: 'ap-south-1_rTB7At04j', // e.g. us-east-2_uXboG5pAb
+userPoolClientId: '67cnntm9ctbsum8d6jvh8q9f17', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+region: 'ap-south-1' // e.g. us-east-2
+},
     api: {
         invokeUrl: 'https://zsievbkdqg.execute-api.ap-south-1.amazonaws.com/sbi' // Replace with your API URL
     }
 };
+
 
 
