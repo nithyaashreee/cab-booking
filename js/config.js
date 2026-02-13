65dd348d38e6e873844b356ba3740e47dbfbb440
Nithyashree D <nithyashreed777@gmail.com>
2026-02-13 05:50:49 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-userPoolId: 'ap-south-1_rTB7At04j', // e.g. us-east-2_uXboG5pAb
-userPoolClientId: '67cnntm9ctbsum8d6jvh8q9f17', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-region: 'ap-south-1' // e.g. us-east-2
-},
+        userPoolId: 'ap-south-1_rTB7At04j', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '67cnntm9ctbsum8d6jvh8q9f17', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
     api: {
-        invokeUrl: '' // Replace with your API URL
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
